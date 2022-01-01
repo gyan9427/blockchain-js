773d960576414e4e4db842ddd92ca4b68a792929
gyan9427 <gyan9427@gmail.com>
2022-01-01 21:19:48 +0530

    Create createNewBlock method to generate a newBlock

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,6 +1,22 @@
 class BlockChain {
     constructor() {
         this.chain = [];
-        this.transactions = [];
+        this.newTransactions = [];
+    }
+
+    createNewBlock(nonce,previousBlockHash,hash){
+        const newBlock = {
+            index: this.chain.length +1,
+            timestamp: Date.now(),
+            transactions: this.newTransactions,
+            nonce: nonce,
+            previousBlockHash: previousBlockHash,
+            hash: hash      //These are the transactions within the new blockchains   
+        };
+
+        this.newTransactions = [];
+        this.chain.push(newBlock);
+
+        return newBlock;
     }
 }
