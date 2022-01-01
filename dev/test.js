9723cd939ce954c17446b5cac56381de4af7cd75
gyan9427 <gyan9427@gmail.com>
2022-01-01 21:44:29 +0530

    write a test to check the functionality of the getLastBlock method

--- dev/test.js
+++ dev/test.js
@@ -4,4 +4,8 @@ const bitCoin = new BlockChain();
 bitCoin.createNewBlock(123,'SDF23432KLKS','59849KJHF8SDB2')
 bitCoin.createNewBlock(12,'SDF23432KLKS234','59849KJHF8SFSFDSDB2')
 bitCoin.createNewBlock(2334,'SDFSFJ3020','FKJH9990BN')
+
+let lastBlock = bitCoin.getLastBlock();
+
 console.log(bitCoin);
+console.log(lastBlock);
