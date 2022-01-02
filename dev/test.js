4a57ee4157e04972c3c8dcaa059425054f9749c4
gyan9427 <gyan9427@gmail.com>
2022-01-02 10:16:08 +0530

    create test to test hashBlock function

--- dev/test.js
+++ dev/test.js
@@ -1,14 +1,38 @@
 const BlockChain = require ('./blockchain')
 
 const bitCoin = new BlockChain();
-bitCoin.createNewBlock(123,'SDF23432KLKS','59849KJHF8SDB2')
-bitCoin.createTransaction(234,'gyan','soumya')
-bitCoin.createNewBlock(12,'SDF23432KLKS234','59849KJHF8SFSFDSDB2')
+// bitCoin.createNewBlock(123,'SDF23432KLKS','59849KJHF8SDB2')
+// bitCoin.createTransaction(234,'gyan','soumya')
+// bitCoin.createNewBlock(12,'SDF23432KLKS234','59849KJHF8SFSFDSDB2')
 // bitCoin.createNewBlock(2334,'SDFSFJ3020','FKJH9990BN')
 
 
 
-console.log(bitCoin);
-console.log(bitCoin.chain[1]['transactions']);
+
+// console.log(bitCoin);
+// console.log(bitCoin.chain[1]['transactions']);
 
 // console.log(lastBlock);
+
+
+const previousBlockHash = 'ASD23980KLKJAS090';
+const nonce = 908;
+const currentBlockData = [
+    {
+        amount: 20,
+        sender: 'LKK76876JNDSLJKJ',
+        recipient: 'KHJH99789JHJBKBT'
+    },
+    {
+        amount: 209,
+        sender: 'ASD76876JNDSLJKJ',
+        recipient: 'HKJHJHL89JHJBKBT'
+    },
+    {
+        amount: 120,
+        sender: 'LLOOJ76876JNDSLJKJ',
+        recipient: 'NBNBNB99789JHJBKBT'
+    },
+];
+
+console.log(bitCoin.hashBlock(previousBlockHash,currentBlockData,nonce));
