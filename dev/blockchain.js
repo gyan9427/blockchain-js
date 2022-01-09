208e4ae32b784c3e4389807178d168465818927f
gyan9427 <gyan9427@gmail.com>
2022-01-09 16:36:22 +0530

    minute changes in chainIsValid method

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -70,23 +70,22 @@ class BlockChain {
     chainIsValid(blockchain){
         let validChain = true;
 
-        for(var i=1;blockchain.length;i++){
+        for (var i = 1; i < blockchain.length; i++) {
             const currentBlock = blockchain[i];
-            const prevBlock = blockchain[i-1];
-            const blockHash = this.hashBlock(prevBlock['hash'],{transactions: currentBlock['transactions'],index:currentBlock[index]}, currentBlock['nonce']);
-
-            if (blockHash.substring(0,4) !== "0000") validChain = false;
-            if(currentBlock['previousBlockHash'] !== prevBlock['hash']) validChain = false;
+            const prevBlock = blockchain[i - 1];
+            const blockHash = this.hashBlock(prevBlock['hash'], { transactions: currentBlock['transactions'], index: currentBlock['index'] }, currentBlock['nonce']);
+            if (blockHash.substring(0, 4) !== '0000') validChain = false;
+            if (currentBlock['previousBlockHash'] !== prevBlock['hash']) validChain = false;
         };
-
+    
         const genesisBlock = blockchain[0];
         const correctNonce = genesisBlock['nonce'] === 100;
         const correctPreviousBlockHash = genesisBlock['previousBlockHash'] === '0';
         const correctHash = genesisBlock['hash'] === '0';
         const correctTransactions = genesisBlock['transactions'].length === 0;
-
-        if (!correctHash || !correctNonce || !correctPreviousBlockHash || !correctTransactions ) validChain = false;
-
+    
+        if (!correctNonce || !correctPreviousBlockHash || !correctHash || !correctTransactions) validChain = false;
+    
         return validChain;
     }
 }
